c1a91bfb87b369634dac81c76f5a537608fa9b4c
glgerard <glgerard@gmail.com>
2016-12-21 09:01:27 +0100

    Added a version with a DBN instead of an RBM for Somatic Mutations.

--- data/AML/common_pat_id.sh
+++ data/AML/common_pat_id.sh
@@ -4,9 +4,11 @@
 
 cut -d',' -f1 AML_gene_espression2.csv | uniq | sort | uniq > AML_gene_expression_pat_id.txt
 cut -d',' -f1 AML_miRNA_Seq2.csv | uniq | sort | uniq > AML_miRNA_Seq_pat_id.txt
-cut -d',' -f1 AML_somatic_mutations2.txt | uniq | sort | uniq > AML_somatic_mutations_pat_id.txt
+cut -d$'\t' -f1 Patient_MutatedGenes_somatic.NoNorm.txt | uniq | sort | uniq > AML_somatic_mutations_pat_id.txt
 
 # we loop on AML_somatic_mutations as it has the largest number of patient id
+[ -f pat_id.txt ] & rm pat_id.txt
+echo "pat_id" > pat_id.txt
 for p in `cat AML_somatic_mutations_pat_id.txt`; do
   echo $p;
   if [ `grep -c $p AML_gene_expression_pat_id.txt` == 1 ]; then
