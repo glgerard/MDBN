067519a673124d81a8d96a38db420de74c5b3312
Gianluca Gerard <glgerard@gmail.com>
2017-01-13 21:04:37 +0100

    Removed outdated comments.

--- src/AMLsm.py
+++ src/AMLsm.py
@@ -4,7 +4,7 @@ import datetime
 import numpy
 import theano
 
-from MDBN import train_bottom_layer
+from MDBN import train_dbn
 from MDBN import train_top
 from MDBN import DBN
 
@@ -61,30 +61,17 @@ def train_AML_MDBN(datafiles,
                                                         graph_output=graph_output,
                                                         datadir=datadir)
 
-#    dm_DBN, output_DM_t_set, output_DM_v_set = train_DM(datafiles['DM'],
-#                                                        rng,
-#                                                        holdout=holdout,
-#                                                        repeats=repeats,
-#                                                        lambda_1=0.01,
-#                                                        lambda_2=0.1,
-#                                                        graph_output=graph_output,
-#                                                        datadir=datadir)
-
     print('*** Training on joint layer ***')
 
     output_ME_t_set, output_ME_v_set = me_DBN.MLP_output_from_datafile(datafiles['ME'], holdout=holdout, repeats=repeats)
     output_GE_t_set, output_GE_v_set = ge_DBN.MLP_output_from_datafile(datafiles['GE'], holdout=holdout, repeats=repeats)
     output_SM_t_set, output_SM_v_set = sm_DBN.MLP_output_from_datafile(datafiles['SM'], holdout=holdout, repeats=repeats)
 
-    # output_DM_t_set, output_DM_v_set = dm_DBN.MLP_output_from_datafile(datafiles['DM'], holdout=holdout, repeats=repeats)
-
     joint_train_set = theano.shared(numpy.concatenate([
-    #               output_ME_t_set, output_GE_t_set, output_DM_t_set],axis=1), borrow=True)
                     output_ME_t_set, output_GE_t_set, output_SM_t_set], axis = 1), borrow = True)
 
     if holdout > 0:
         joint_val_set = theano.shared(numpy.concatenate([
-    #                        output_ME_v_set, output_GE_v_set, output_DM_v_set],axis=1), borrow=True)
                             output_ME_v_set, output_GE_v_set, output_SM_v_set],axis=1), borrow=True)
     else:
         joint_val_set = None
@@ -97,14 +84,10 @@ def train_AML_MDBN(datafiles,
     GE_output, _ = ge_DBN.MLP_output_from_datafile(datafiles['GE'])
     SM_output, _ = sm_DBN.MLP_output_from_datafile(datafiles['SM'])
 
-#    DM_output, _ = dm_DBN.MLP_output_from_datafile(datafiles['DM'])
-
-#    joint_output = theano.shared(numpy.concatenate([ME_output, GE_output, DM_output],axis=1), borrow=True)
     joint_output = theano.shared(numpy.concatenate([ME_output, GE_output, SM_output],axis=1), borrow=True)
 
     classes = top_DBN.get_output(joint_output)
 
-#    save_network(classes, ge_DBN, me_DBN, dm_DBN, top_DBN, holdout, output_file, output_folder, repeats)
     save_network(classes, ge_DBN, me_DBN, sm_DBN, None, top_DBN, holdout, output_file, output_folder, repeats)
 
     return classes
@@ -139,13 +122,6 @@ def save_network(classes, ge_DBN, me_DBN, sm_DBN, dm_DBN, top_DBN, holdout, outp
                     'batch_size': 20,
                     'k': 10
                 },
-#                dm_config={
-#                    'number_of_nodes': dm_DBN.number_of_nodes(),
-#                    'epochs': [8000, 800],
-#                    'learning_rate': [0.005, 0.1],
-#                    'batch_size': 20,
-#                    'k': 1
-#                },
                 top_config={
                     'number_of_nodes': top_DBN.number_of_nodes(),
                     'epochs': [800, 800],
@@ -186,12 +162,6 @@ def load_network(input_file, input_folder):
     sm_DBN = DBN(n_ins=layer_sizes[0], hidden_layers_sizes=layer_sizes[1:-1], n_outs=layer_sizes[-1],
                   W_list=[params[0]['W']],b_list=[params[1]['b']])
 
-#    config = npz['dm_config'].tolist()
-#    params = npz['dm_params']
-#    layer_sizes = config['number_of_nodes']
-#    dm_DBN = DBN(n_ins=layer_sizes[0], hidden_layers_sizes=layer_sizes[1:-1], n_outs=layer_sizes[-1],
-#                  W_list=[params[0]['W'],params[2]['W']],b_list=[params[1]['b'],params[3]['b']])
-
     config = npz['top_config'].tolist()
     params = npz['top_params']
     layer_sizes = config['number_of_nodes']
@@ -201,7 +171,6 @@ def load_network(input_file, input_folder):
 
     os.chdir(root_dir)
 
-#    return (me_DBN, ge_DBN, dm_DBN, top_DBN)
     return (me_DBN, ge_DBN, sm_DBN, None, top_DBN)
 
 def train_DM(datafile,
@@ -224,20 +193,18 @@ def train_DM(datafile,
                                                        clip=clip,
                                                        holdout=holdout,
                                                        repeats=repeats,
-#                                                       transform_fn=numpy.power,
-#                                                       exponent=1.0/6.0,
                                                        datadir=datadir)
 
-    return train_bottom_layer(train_set, validation_set,
-                              batch_size=batch_size,
-                              k=k,
-                              layers_sizes=layers_sizes,
-                              pretraining_epochs=pretraining_epochs,
-                              pretrain_lr=pretrain_lr,
-                              lambda_1=lambda_1,
-                              lambda_2=lambda_2,
-                              rng=rng,
-                              graph_output=graph_output)
+    return train_dbn(train_set, validation_set,
+                     batch_size=batch_size,
+                     k=k,
+                     layers_sizes=layers_sizes,
+                     pretraining_epochs=pretraining_epochs,
+                     pretrain_lr=pretrain_lr,
+                     lambda_1=lambda_1,
+                     lambda_2=lambda_2,
+                     rng=rng,
+                     graph_output=graph_output)
 
 def train_GE(datafile,
              rng,
@@ -261,16 +228,16 @@ def train_GE(datafile,
                                                        repeats=repeats,
                                                        datadir=datadir)
 
-    return train_bottom_layer(train_set, validation_set,
-                              batch_size=batch_size,
-                              k=k,
-                              layers_sizes=layers_sizes,
-                              pretraining_epochs=pretraining_epochs,
-                              pretrain_lr=pretrain_lr,
-                              lambda_1=lambda_1,
-                              lambda_2=lambda_2,
-                              rng=rng,
-                              graph_output=graph_output)
+    return train_dbn(train_set, validation_set,
+                     batch_size=batch_size,
+                     k=k,
+                     layers_sizes=layers_sizes,
+                     pretraining_epochs=pretraining_epochs,
+                     pretrain_lr=pretrain_lr,
+                     lambda_1=lambda_1,
+                     lambda_2=lambda_2,
+                     rng=rng,
+                     graph_output=graph_output)
 
 def train_ME(datafile,
              rng,
@@ -294,16 +261,16 @@ def train_ME(datafile,
                                                        repeats=repeats,
                                                        datadir=datadir)
 
-    return train_bottom_layer(train_set, validation_set,
-                                batch_size=batch_size,
-                                k=k,
-                                layers_sizes=layers_sizes,
-                                pretraining_epochs=pretraining_epochs,
-                                pretrain_lr=pretrain_lr,
-                                lambda_1=lambda_1,
-                                lambda_2=lambda_2,
-                                rng=rng,
-                                graph_output=graph_output)
+    return train_dbn(train_set, validation_set,
+                     batch_size=batch_size,
+                     k=k,
+                     layers_sizes=layers_sizes,
+                     pretraining_epochs=pretraining_epochs,
+                     pretrain_lr=pretrain_lr,
+                     lambda_1=lambda_1,
+                     lambda_2=lambda_2,
+                     rng=rng,
+                     graph_output=graph_output)
 
 def train_SM(datafile,
              rng,
@@ -327,21 +294,20 @@ def train_SM(datafile,
                                                        repeats=repeats,
                                                        datadir=datadir)
 
-    return train_bottom_layer(train_set, validation_set,
-                                batch_size=batch_size,
-                                k=k,
-                                layers_sizes=layers_sizes,
-                                pretraining_epochs=pretraining_epochs,
-                                pretrain_lr=pretrain_lr,
-                                lambda_1=lambda_1,
-                                lambda_2=lambda_2,
-                                rng=rng,
-                                graph_output=graph_output)
+    return train_dbn(train_set, validation_set,
+                     batch_size=batch_size,
+                     k=k,
+                     layers_sizes=layers_sizes,
+                     pretraining_epochs=pretraining_epochs,
+                     pretrain_lr=pretrain_lr,
+                     lambda_1=lambda_1,
+                     lambda_2=lambda_2,
+                     rng=rng,
+                     graph_output=graph_output)
 
 def prepare_AML_TCGA_datafiles(datadir='data'):
     datafiles = {
         'GE': 'AML/AML_gene_expression_table2.csv',
-#        'DM': 'AML/3.Methylation_0.5.out',
         'ME': 'AML/AML_miRNA_Seq_table2.csv',
         'SM': 'AML/AML_somatic_mutations_table2.csv'
     }
@@ -372,6 +338,3 @@ if __name__ == '__main__':
     numpy.savez('Results_%s.npz' % run_start_date_str,
                 results=results)
     os.chdir(root_dir)
-
-#    train_ME(datafiles['ME'],graph_output=True)
-#    train_GE(datafiles['GE'],graph_output=True)
